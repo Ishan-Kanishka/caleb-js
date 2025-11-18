ec4b2e8ef6fe41894399f2ed1373f988899a151b
Ishan-Kanishka <ikanishka368@gmail.com>
2025-11-18 17:26:28 +0530

    defer

--- js/youtube.js
+++ js/youtube.js
@@ -1,1 +1,4 @@
-alert("Hello World");
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Order Placed. Check email for confirmation";
+    document.getElementById("button").style.display = "none";
+}
